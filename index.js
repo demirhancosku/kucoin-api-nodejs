11b5b4228f624a72065576bf6932ea3ca53b977e
Arda Kilicdagi <ardakilicdagi@gmail.com>
2017-11-04 19:45:10 +0300

    * License added * Multi Language support * Code cleaned up and comments translated * More configuration parameters are put so index.js doesn't need modification for e.g: limit changes. * To check a lower amount, instead of multiplying by 2, now it sums it with a minimum_amount new variable. If the delimiter is bigger, readjusting may put order too low or high.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Kucoin = require('./src/api'),
     config = require('./config');
 
-let kucoin = new Kucoin(config._api_key, config._api_secret, config._api_latency),
+const lang = require('./src/lang/' + config.lang);
+
+let kucoin = new Kucoin(config.api.key, config.api.secret, config.api.latency),
     data = {},
-    pricedelimiter = 0.00000001,
-    amount = 2000,
+    pricedelimiter = config.orders.price_delimiter,
+    amount = config.orders.amount,
     summary = [],
-    symbol = config._api_pair;
+    symbol = config.api.pair,
+    minimum_amount = 0.00000001; //This is the minimum amount for increasing or decreasing the price
 
 data.container = {
     last_sell: 0,
@@ -31,12 +34,12 @@ let setBuy = async () => {
 
 let setOrders = async () => {
 
-    //Eski sell orderı temizle
+    //Cancel the last sell order
     if (data.active_orders.SELL.length > 0) {
         await kucoin.cancel_order('SELL', symbol, data.active_orders.SELL[0][5]);
     }
 
-    //Eski buy orderı temizle
+    //Cancel the last buy order
     if (data.active_orders.BUY.length > 0) {
         await kucoin.cancel_order('BUY', symbol, data.active_orders.BUY[0][5]);
     }
@@ -47,30 +50,29 @@ let setOrders = async () => {
     let sellOrder = await setSell();
 
 
-    //Satış order'ı gerçekleştirilmiş mi
+    //Did the sell order fullfill?
     if (sellOrder.orderOid === undefined && buyOrder.orderOid !== undefined) {
-        summary.push('Satış için yeterli miktar yok');
-
+        summary.push(lang.not_enough_amount_for_sell);
 
         await kucoin.cancel_order('BUY', symbol, buyOrder.orderOid);
-        summary.push('Alış emri iptal');
+        summary.push(lang.buy_order_canceled);
 
     }
 
-    //Alış order'ı gerçekleştirilmiş mi
+    //Did the buy order fullfill?
     if (buyOrder.orderOid === undefined && sellOrder.orderOid !== undefined) {
 
-        summary.push('Alış için yeterli miktar yok');
+        summary.push(lang.not_enough_amount_for_buy);
 
         await kucoin.cancel_order('SELL', symbol, sellOrder.orderOid);
-        summary.push('Satış emri iptal');
+        summary.push(lang.sell_order_canceled);
 
     }
 
 }
 
 let trader = async () => {
-    console.log('----------------------');
+    console.log(lang.delimiter);
     summary = [];
 
     //Get current ticket
@@ -81,98 +83,91 @@ let trader = async () => {
     data.active_orders = await kucoin.active_orders(symbol);
 
 
-    //aktif order yoksa
+    //If there isn't no active order, we'll try to make a new pair
     if (data.active_orders.BUY.length === 0 && data.active_orders.SELL.length === 0) {
 
-        summary.push('Aktif order yok');
+        summary.push(lang.no_active_orders);
 
 
-        //iki fiyat arası uygun boşluk var mı?
-        if (data.ticker.sell - data.ticker.buy > (pricedelimiter * 2)) {
+        //Is there an available slot to put order pairs in market?
+        if (data.ticker.sell - data.ticker.buy > (pricedelimiter + minimum_amount)) { //(pricedelimiter * 2)
 
-            summary.push('Fiyat aralığı uygun');
+            summary.push(lang.market_prices_available_for_order_pair);
 
             await setOrders();
 
         } else {
-            summary.push('Fiyat aralığı uygun değil');
+            summary.push(lang.market_prices_not_available_for_order_pair);
         }
 
-    } else if ( //iki order da var ve henüz gerçekleşmemiş
-    data.active_orders.SELL.length > 0 &&
-    data.active_orders.BUY.length > 0 &&
-    data.active_orders.SELL[0][4] === 0 &&
-    data.active_orders.BUY[0][4] === 0) {
+    } else if ( //There are two unfullfilled orders
+        data.active_orders.SELL.length > 0
+        && data.active_orders.BUY.length > 0
+        && data.active_orders.SELL[0][4] === 0
+        && data.active_orders.BUY[0][4] === 0
+    ) {
+        summary.push(lang.there_are_two_unfullfilled_orders);
 
-        summary.push('Henüz işlem görmemiş iki order var');
-
-        //herhangi bir orderının önüne geçen bir order var mı
+        //Is there a lower or higher price for either one of the pairs ?
         if ((data.active_orders.SELL[0][2] > data.ticker.sell) || (data.active_orders.BUY[0][2] < data.ticker.buy)) {
-            summary.push('Orderları yeniden sıralıyoruz');
-            if (data.ticker.sell - data.ticker.buy > (pricedelimiter * 2)) {
-                summary.push('Yeniden sıralama için fiyat aralığı uygun');
-
+            summary.push(lang.re_sorting_the_pair);
+            if (data.ticker.sell - data.ticker.buy > (pricedelimiter + minimum_amount)) {  //(pricedelimiter * 2)
+                summary.push(lang.price_range_is_okay_for_re_sorting);
                 await setOrders();
             } else {
-                summary.push('Yeniden sıralama için fiyat aralığı uygun değil');
-
-
+                summary.push(lang.price_range_is_not_okay_for_re_sorting);
                 await kucoin.cancel_order('SELL', symbol, data.active_orders.SELL[0][5]);
                 await kucoin.cancel_order('BUY', symbol, data.active_orders.BUY[0][5]);
-                summary.push('Sıralama yapılamadığı için orderlar iptal edildi');
+                summary.push(lang.orders_are_canceled_because_cannot_re_sort);
             }
-
         }
+        //Is there a lower or higher price for either one of the pairs ?
 
 
-        //orderların altında herhangi bir boşluk varmı
+    } else if ( //Was the sell order left alone?
+        data.active_orders.SELL.length > 0
+        && data.active_orders.BUY.length === 0
+    ) {
 
-
-    } else if ( //satış yalnız kaldıysa
-    data.active_orders.SELL.length > 0 && data.active_orders.BUY.length === 0) {
-
-        //boşluk varsa yerine taşı
-        summary.push('Satış yalnız kaldı :( ');
+        //If there is an available space for sell, move it to there
+        summary.push(lang.sell_order_left_alone);
 
         if ((data.active_orders.SELL[0][2] > data.ticker.sell)) {
 
-            summary.push('Satış için yer değiştirme gerekiyor.');
+            summary.push(lang.sell_needs_to_be_adjusted);
 
             if ((data.container.last_buy < data.ticker.sell - pricedelimiter) && data.container.last_buy !== 0) {
-                summary.push('Satış yer değiştiryoruz');
+                summary.push(lang.readjusting_sell_order);
                 await kucoin.cancel_order('SELL', symbol, data.active_orders.SELL[0][5]);
                 await kucoin.create_order('SELL', symbol, data.ticker.sell - pricedelimiter, amount);
             } else {
-                console.log( data.container.last_buy , data.container.last_buy !== 0);
-
-                summary.push('Satış yer değiştiremiyoruz');
+                console.log(data.container.last_buy, data.container.last_buy !== 0);
+                summary.push(lang.cannot_readjust_sell_order);
             }
 
         }
 
-    } else if ( //alış yalnız kaldıysa
-    data.active_orders.BUY.length > 0 && data.active_orders.SELL.length === 0) {
+    } else if ( //Was the buy order left alone
+        data.active_orders.BUY.length > 0
+        && data.active_orders.SELL.length === 0
+    ) {
 
-        //boşluk varsa yerine taşı
-        summary.push('Alış yalnız kaldı :( ');
+        //If there is an available space for sell, move it to there
+        summary.push(lang.buy_order_left_alone);
 
         if (data.active_orders.BUY[0][2] < data.ticker.buy) {
-
-            summary.push('Alış için değiştirmek gerekiyor');
-
-
+            summary.push(lang.buy_needs_to_be_adjusted);
             if ((data.container.last_sell > data.ticker.buy + pricedelimiter) && data.container.last_sell !== 0) {
-                summary.push('Alış yer değiştiryoruz');
+                summary.push(lang.readjusting_buy_order);
                 await kucoin.cancel_order('BUY', symbol, data.active_orders.BUY[0][5]);
                 await kucoin.create_order('BUY', symbol, data.ticker.buy + pricedelimiter, amount);
             } else {
-                console.log(data.container.last_sell  , data.container.last_sell !== 0);
-                summary.push('Alış yer değiştiremiyoruz');
+                console.log(data.container.last_sell, data.container.last_sell !== 0);
+                summary.push(lang.cannot_readjust_buy_order);
             }
         }
 
     }
-
 
     console.log(summary.reverse());
     await trader();
